7c8dae8158fe96b9a1eaa04c053e884ea77bf19d
zahir791432 <zguercio@alumno.huergo.edu.ar>
2026-04-08 11:45:22 -0300

    fondo
    ondo

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
 
-ventana = tk.tk()
+ventana = tk.Tk()
+ventana.title("Spotify 2")
+ventana.geometry("1920x1080")
+ventana.configure(bg="#19191a")
+ventana.mainloop()
