6e25119aeb3a56a183f5456ba8be21729badeb21
zahir791432 <zguercio@alumno.huergo.edu.ar>
2026-04-15 14:42:33 -0300

    okay
    hola

--- interfaz.py
+++ interfaz.py
@@ -2,8 +2,50 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 from PIL import Image, ImageTk 
+from main import ventana
 
 def ventanas_en_pantalla(ventana):
     label = ttk.Label(ventana, text="Hola Mundo", font=("Arial", 12))
     label.pack(pady=20)
 
+def imagenes(type):
+    nombre = f"assets/{type}.png"
+    img = Image.open(nombre)
+    img = img.resize((40, 40))  
+    img_tk = ImageTk.PhotoImage(img)
+    return img_tk
+
+img_play = imagenes("play")
+img_pause = imagenes("pause")
+img_atrasar = imagenes("atrasar")
+img_adelantar = imagenes("adelantar")
+
+boton_play = tk.Button(
+    ventana,
+    image=img_play,
+    bg="#0077b6",
+    activebackground="#0077b6",
+    borderwidth=0,
+    highlightthickness=0)
+
+boton_play.place(x=670, y=500)
+
+boton_adelantar = tk.Button(
+    ventana,
+    image=img_adelantar,
+    bg="#0077b6",
+    activebackground="#0077b6",
+    borderwidth=0,
+    highlightthickness=0)
+
+boton_adelantar.place(x=770, y=500)
+
+boton_atrasar = tk.Button(
+    ventana,
+    image=img_atrasar,
+    bg="#0077b6",
+    activebackground="#0077b6",
+    borderwidth=0,
+    highlightthickness=0)
+
+boton_atrasar.place(x=570
--- main.py
+++ main.py
@@ -2,52 +2,17 @@ import tkinter as tk
 from tkinter import messagebox, ttk
 from tkinter import *
 from PIL import Image, ImageTk
+from interfaz import boton_play, boton_adelantar, boton_atrasar
 
 ventana = tk.Tk()
 ventana.title("Spotify 2")
 ventana.geometry("1920x1080")
 ventana.configure(bg="#0077b6")
 
-def imagenes(type):
-    nombre = f"assets/{type}.png"
-    img = Image.open(nombre)
-    img = img.resize((40, 40))  
-    img_tk = ImageTk.PhotoImage(img)
-    return img_tk
 
-img_play = imagenes("play")
-img_pause = imagenes("pause")
-img_atrasar = imagenes("atrasar")
-img_adelantar = imagenes("adelantar")
-
-boton_play = tk.Button(
-    ventana,
-    image=img_play,
-    bg="#0077b6",
-    activebackground="#0077b6",
-    borderwidth=0,
-    highlightthickness=0)
 
 boton_play.place(x=670, y=500)
-
-boton_adelantar = tk.Button(
-    ventana,
-    image=img_adelantar,
-    bg="#0077b6",
-    activebackground="#0077b6",
-    borderwidth=0,
-    highlightthickness=0)
-
 boton_adelantar.place(x=770, y=500)
-
-boton_atrasar = tk.Button(
-    ventana,
-    image=img_atrasar,
-    bg="#0077b6",
-    activebackground="#0077b6",
-    borderwidth=0,
-    highlightthickness=0)
-
 boton_atrasar.place(x=570, y=500)
 
 ventana.mainloop()
